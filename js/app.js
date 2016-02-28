7d8f896335a487cea88bb4e6d2c50e8eb730026d
caraclarke <cara.clarke8@gmail.com>
2016-02-27 23:01:53 -0500

    Updated index, deleted
    css

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 (function starWarsApp() {
-	var starWarsApp = angular.module('starWarsApp', ['ngRoute', 'xyz.angular.swapi']);
+	var starWarsApp = angular.module('starWarsApp', ['ngRoute']);
 
 	starWarsApp.config(function($routeProvider, $httpProvider) {
-		$httpProvider.defaults.withCredentials = true;
+		// $httpProvider.defaults.withCredentials = true;
 		$routeProvider
 			.when('/', {
 				controller: 'treeController',
 				controllerAs: 'tIndexCtrl',
 				templateUrl: 'js/views/treeIndex.html'
+			})
+      .when('/people/:personId', {
+				controller: 'personShowController',
+				controllerAs: 'tShowCtrl',
+				templateUrl: 'js/views/personView.html'
 			})
 		.otherwise({ redirectTo: '/' });
 	});
